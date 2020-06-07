689ddd7fbe147291e92b8bd8e1e27354e7c9a5e3
Levin Li <3259963@qq.com>
2020-06-07 10:41:15 +0000

    add rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,20 @@
 module.exports = {
-    "env": {
-        "commonjs": true,
-        "es2020": true,
-        "node": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/eslint-recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": 11
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
+  env: {
+    commonjs: true,
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 11,
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-non-null-assertion': 0,
+  },
 };
